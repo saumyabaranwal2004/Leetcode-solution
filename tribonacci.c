7266e2e11e76b98e72b85b683e9a7104f1109206
Saumya Baranwal <saumyabaranwal2004@gmail.com>
2025-11-20 19:44:13 +0530

    Implement tribonacci function

---
+++ tribonacci.c
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+       if(n==0){
+        return 0;
+       } 
+       if(n==1 || n==2){
+        return 1;
+       }
+       else{
+        int firstterm=0;
+        int secondterm=1;
+        int thirdterm=1;
+        for(int i=3;i<=n;i++){
+            int fourthterm=firstterm+secondterm+thirdterm;
+            firstterm=secondterm;
+            secondterm=thirdterm;
+            thirdterm=fourthterm;
+        }
+        return thirdterm;
+       }
+    }  
+};
