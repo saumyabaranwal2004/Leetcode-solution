85356c1198d21ceff8291da704e11d19e62a14ea
Saumya Baranwal <saumyabaranwal2004@gmail.com>
2025-11-20 19:45:53 +0530

    Implement romanToInt function

---
+++ romantoInteger.c
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map<char, int> val = {
+            {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, 
+            {'C', 100}, {'D', 500}, {'M', 1000}
+        };
+        int total = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int current = val[s[i]];
+            if (i + 1 < s.length() && current < val[s[i + 1]]) {
+                total -= current;
+            } else {
+                total += current;
+            }
+        }
+        return total; 
+    }
+};
